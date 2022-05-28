07a89d3838a6927308cf10c08c10a544264a1ce6
GrCOTE7 <GrCOTE7@gmail.com>
2022-05-28 15:35:02 +0200

    Tr4 OK

--- plugins/grcote7/recaptcha/components/ReCaptcha4.php
+++ plugins/grcote7/recaptcha/components/ReCaptcha4.php
@@ -1,25 +1,37 @@
-<?php namespace GrCOTE7\ReCaptcha\Components;
+<?php
+
+/**
+ * ReCaptcha v3 - 2022.
+ */
+
+namespace GrCOTE7\ReCaptcha\Components;
 
 use Cms\Classes\ComponentBase;
 
 class ReCaptcha4 extends ComponentBase
 {
-    /**
-     * Gets the details for the component
-     */
-    public function componentDetails()
-    {
-        return [
-            'name'        => 'ReCaptcha4 Component',
-            'description' => 'No description provided yet...'
-        ];
-    }
+	/**
+	 * Gets the details for the component.
+	 */
+	public function componentDetails()
+	{
+		return [
+			'name'        => 'ReCaptcha4 Component',
+			'description' => 'No description provided yet...',
+		];
+	}
 
-    /**
-     * Returns the properties provided by the component
-     */
-    public function defineProperties()
-    {
-        return [];
-    }
+	/**
+	 * Returns the properties provided by the component.
+	 */
+	public function defineProperties()
+	{
+		return [];
+	}
+
+	public function onSubscribe()
+	{
+		// Here code for submision...
+		$this->page['result'] = 'Screen after submit...';
+	}
 }
--- plugins/grcote7/recaptcha/components/ReCaptcha5.php
+++ plugins/grcote7/recaptcha/components/ReCaptcha5.php
@@ -1,25 +1,37 @@
-<?php namespace GrCOTE7\ReCaptcha\Components;
+<?php
+
+/**
+ * ReCaptcha v3 - 2022.
+ */
+
+namespace GrCOTE7\ReCaptcha\Components;
 
 use Cms\Classes\ComponentBase;
 
 class ReCaptcha5 extends ComponentBase
 {
-    /**
-     * Gets the details for the component
-     */
-    public function componentDetails()
-    {
-        return [
-            'name'        => 'ReCaptcha5 Component',
-            'description' => 'No description provided yet...'
-        ];
-    }
+	/**
+	 * Gets the details for the component.
+	 */
+	public function componentDetails()
+	{
+		return [
+			'name'        => 'ReCaptcha5 Component',
+			'description' => 'No description provided yet...',
+		];
+	}
 
-    /**
-     * Returns the properties provided by the component
-     */
-    public function defineProperties()
-    {
-        return [];
-    }
+	/**
+	 * Returns the properties provided by the component.
+	 */
+	public function defineProperties()
+	{
+		return [];
+	}
+
+	public function onSubscribe()
+	{
+		// Here code for submision...
+		$this->page['result'] = 'Screen after submit...';
+	}
 }
